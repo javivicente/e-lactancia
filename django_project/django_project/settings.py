7775589587bdfcd3a7ac9d312adc8135e0e9d0a7
Javier Vicente <javier.vicente.r@gmail.com>
2015-06-29 11:03:29 -0400

    adding e-lactancia.org to allowed hosts

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
-ALLOWED_HOSTS = ['188.166.122.21', '0.0.0.0:9000', '127.0.0.1:9000',]
+ALLOWED_HOSTS = ['188.166.122.21', '0.0.0.0:9000', '127.0.0.1:9000','e-lactancia.org', 'www.e-lactancia.org',u'e-lactancia.org',u'www.e-lactancia.org']
 
 LANGUAGE_CODE = 'es-ES'
 
